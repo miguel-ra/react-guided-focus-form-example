5e1d5723fc1a20eea7dbe4e4597760c0cdca9c8b
miguel-ra <miguel-ra@users.noreply.github.com>
2020-10-04 04:18:03 +0000

    Deploying to gh-pages from  @ 98e043f4ed02d1e3a0e8dc5d90d76953a7b505a2 🚀

---
+++ precache-manifest.8ef77bda6c3ecd47a24975bada711140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95694579323be177924369821cd520f5",
+    "url": "/react-guided-focus-form-example/index.html"
+  },
+  {
+    "revision": "3657747ede87801a4ca2",
+    "url": "/react-guided-focus-form-example/static/css/main.1a65db3d.chunk.css"
+  },
+  {
+    "revision": "b3b4a6b910f8ab8a3247",
+    "url": "/react-guided-focus-form-example/static/js/2.ea0a9d74.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/react-guided-focus-form-example/static/js/2.ea0a9d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3657747ede87801a4ca2",
+    "url": "/react-guided-focus-form-example/static/js/main.8b6177b1.chunk.js"
+  },
+  {
+    "revision": "dc239267ab4351ae942a",
+    "url": "/react-guided-focus-form-example/static/js/runtime-main.3948f8f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.095e3abe5f411704dd7afa42d43c73c8.js"
+  "/react-guided-focus-form-example/precache-manifest.8ef77bda6c3ecd47a24975bada711140.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-guided-focus-form-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
